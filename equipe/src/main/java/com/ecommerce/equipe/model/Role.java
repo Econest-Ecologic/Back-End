28f5f405e6366da59f88f2cbb6ba3bbef57126b0
Victor Hugo De Pieri Justino <vdepierejustino@gmail.com>
2025-10-07 16:13:17 -0300

    Criação da Role

---
+++ equipe/src/main/java/com/ecommerce/equipe/model/Role.java
@@ -0,0 +1,23 @@
+package com.ecommerce.equipe.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TBROLES")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CDROLE")
+    private Integer cdRole;
+
+    @Column(name = "NMROLE", unique = true)
+    private String nmRole;
+}
