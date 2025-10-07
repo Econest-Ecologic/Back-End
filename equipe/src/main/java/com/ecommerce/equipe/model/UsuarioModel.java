effdc5e76700502ddb34602a5abebddc61af7055
elessandroabreu <binho_abreu@hotmail.com>
2025-10-07 15:32:48 -0300

    ATUALIZANDO USUARIO

--- equipe/src/main/java/com/ecommerce/equipe/model/UsuarioModel.java
+++ equipe/src/main/java/com/ecommerce/equipe/model/UsuarioModel.java
@@ -12,16 +12,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "TBUSUARIO")
 public class UsuarioModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "IDUSUARIO")
-    private Integer idUsuario;
+    @Column(name = "CDUSUARIO")
+    private Integer cdUsuario;
 
     @Column(name = "NMUSUARIO")
     private String nmUsuario;
 
     @Column(name = "NMEMAIL")
     private String nmEmail;
+
+    @Column(name = "NMSENHA")
+    private String nmSenha;
 
     @Column(name = "NUCPF")
     private String nuCpf;
@@ -32,6 +36,8 @@ public class UsuarioModel {
     @Column(name = "NUTELEFONE")
     private String nuTelefone;
 
+
+    //ALTERACOES ROLOES
     //@Column(name = "TIPO")
     //private enum tipo;
 
