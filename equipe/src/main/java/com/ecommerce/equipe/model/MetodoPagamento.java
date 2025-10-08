fb6fbb099f461159077fa53c96df3c24ee685dd6
Victor Hugo De Pieri Justino <vdepierejustino@gmail.com>
2025-10-08 17:10:46 -0300

    Criação de DTO - RPagamento, Estoque, Avaliação

--- equipe/src/main/java/com/ecommerce/equipe/model/MetodoPagamento.java
+++ equipe/src/main/java/com/ecommerce/equipe/model/MetodoPagamento.java
@@ -1,18 +1,8 @@
 package com.ecommerce.equipe.model;
 
 public enum MetodoPagamento {
-    PIX("Pagamento via Pix"),
-    CARTAO("Pagamento via Cartão"),
-    BOLETO("Pagamento via Boleto");
-
-    private final String descricao;
-
-    MetodoPagamento(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
+    PIX,
+    CARTAO,
+    BOLETO;
 }
 
--- equipe/src/main/java/com/ecommerce/equipe/model/StatusPedido.java
+++ equipe/src/main/java/com/ecommerce/equipe/model/StatusPedido.java
@@ -1,16 +1,9 @@
 package com.ecommerce.equipe.model;
 
 public enum StatusPedido {
-    ABERTO("Pedido aberto"),
-    PAGO("Pagamento confirmado"),
-    ENVIADO("Pedido enviado"),
-    ENTREGUE("Pedido entregue");
-
-    private final String descricao;
-
-    StatusPedido(String descricao) {
-        this.descricao = descricao;
-    }
-
+    ABERTO,
+    PAGO,
+    ENVIADO,
+    ENTREGUE;
 }
 
