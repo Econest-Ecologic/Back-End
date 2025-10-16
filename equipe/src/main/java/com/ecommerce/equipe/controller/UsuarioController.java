693b2df33a3ec6cd875884d7af91004216196c96
Victor Hugo De Pieri Justino <vdepierejustino@gmail.com>
2025-10-16 14:51:49 -0300

    ACriação do usuário controller

--- equipe/src/main/java/com/ecommerce/equipe/controller/UsuarioController.java
+++ equipe/src/main/java/com/ecommerce/equipe/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.ecommerce.equipe.controller;
 
+import com.ecommerce.equipe.dto.UsuarioDto;
 import com.ecommerce.equipe.service.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -15,5 +18,44 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity
+    public ResponseEntity<UsuarioDto> salvar(@RequestBody @Valid UsuarioDto usuarioDto) {
+        UsuarioDto usuario = usuarioService.salvar(usuarioDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDto>> listar() {
+        return ResponseEntity.ok(usuarioService.listarTodos());
+    }
+
+    @GetMapping("/{cdUsuario}")
+    public ResponseEntity<Object> buscar(@PathVariable Integer cdUsuario) {
+        try{
+            UsuarioDto usuario = usuarioService.buscarPorId(cdUsuario);
+            return ResponseEntity.ok(usuario);
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{cdUsuario")
+    public ResponseEntity<Object> atualizar(@PathVariable Integer cdUsuario, @RequestBody @Valid UsuarioDto usuarioDto) {
+        try{
+            UsuarioDto usuario = usuarioService.atualizar(cdUsuario, usuarioDto);
+            return ResponseEntity.ok(usuario);
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{cdUsuario}")
+    public ResponseEntity<String> inativar(@PathVariable Integer cdUsuario) {
+        try{
+            usuarioService.inativar(cdUsuario);
+            return ResponseEntity.ok("Usuario inativado com sucesso");
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 }
